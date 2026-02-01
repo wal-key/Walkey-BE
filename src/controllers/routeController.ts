2623af792e518b3e0100be5403df8fc7447773a4
Jelly <skguswl8038@naver.com>
2026-02-01 20:16:11 +0900

    WAL-30 update: 경로 리스트 조회 api에 400 조건 추가

--- src/controllers/routeController.ts
+++ src/controllers/routeController.ts
@@ -8,9 +8,18 @@ export class RouteController {
   static async getRecommendedRoutes(req: Request, res: Response) {
     const { theme, time } = req.query;
 
+    const parsedTheme = Number(theme);
+    const parsedTime = Number(time);
+
+    if (isNaN(parsedTheme) || isNaN(parsedTime)) {
+      return res.status(400).json({
+        message: '요청을 처리할 수 없습니다. 요청 형식이 올바르지 않습니다.',
+      });
+    }
+
     const routes = await RouteService.getRecommendedRoutes(
-      Number(theme),
-      Number(time)
+      parsedTheme,
+      parsedTime
     );
 
     return res.status(200).json(routes);
