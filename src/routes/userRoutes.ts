d0033ca18821f49ae9de26946adf88f1d2e73ec6
jhlee <jhlee89898989@gmail.com>
2026-02-02 08:52:22 +0900

    WAL-29 remove: 자동생성된 API 임시 주석처리

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -3,6 +3,8 @@ import UserController from '../controllers/userController';
 
 const router = express.Router();
 
+/**
+ * 회원가입, 및 기타 API 임시 주석처리
 // 회원 가입
 router.post('/', UserController.signup);
 
@@ -11,5 +13,6 @@ router.get('/:username', UserController.getUserByUsername);
 
 // 사용자의 산책 세션 목록 조회
 router.get('/:username/sessions', UserController.getUserSessions);
+ */
 
 export default router;
