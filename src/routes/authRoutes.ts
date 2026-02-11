76e75b68580581eca4f1bbc477c18ba05d8bf635
jhlee <jhlee89898989@gmail.com>
2026-02-11 09:17:58 +0900

    WAL-77 update: oauth callback  통합 라우트 추가

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,14 +1,14 @@
 import express from 'express';
 import AuthController from '../controllers/authController';
 import AuthNaverController from '../controllers/authNaverController';
+import OAuthController from '../controllers/oauthController';
 
 const router = express.Router();
 
 // 로그인
 router.post('/login', AuthController.login);
 
-router.get('/login/github', AuthController.githubLogin);
-router.route('/callback/google').get(AuthController.googleSignin);
+router.route('/callback/:provider').get(OAuthController.handlerOauthCallback);
 
 // 네이버
 router.get('/callback/naver', AuthNaverController.naverLogin);
