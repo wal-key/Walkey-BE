e35b0a96b50d0ea5a90082199f2bf5e4366d9381
Jelly <skguswl8038@naver.com>
2026-02-15 00:34:51 +0900

    WAL-76 remove: 사용하지 않는 네이버 callback 라우터 삭제

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -3,14 +3,15 @@ import AuthController from '../controllers/authController';
 import AuthNaverController from '../controllers/authNaverController';
 import OAuthController from '../controllers/oauthController';
 
-const router = express.Router();
+const authRouter = express.Router();
 
 // 로그인
-router.post('/login', AuthController.login);
-router.route('/callback/:provider').get(OAuthController.handlerOauthCallback);
+authRouter.post('/login', AuthController.login);
+authRouter
+  .route('/callback/:provider')
+  .get(OAuthController.handlerOauthCallback);
 
 // 네이버
-router.get('/callback/naver', AuthNaverController.naverLogin);
-router.get('/signin/:provider', AuthController.getSigninUrl);
+authRouter.get('/signin/:provider', AuthController.getSigninUrl);
 
-export default router;
+export default authRouter;
