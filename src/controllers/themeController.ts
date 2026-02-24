e0b2e2acc8dadb4a8c06d2bec91aefc892babfcf
doeun <doroming99@gmail.com>
2026-02-24 13:43:25 +0900

    WAL-92 update : try catch문 제거

--- src/controllers/themeController.ts
+++ src/controllers/themeController.ts
@@ -8,21 +8,16 @@ class ThemeController {
    * 전체 테마 목록 조회
    */
   static getThemes = asyncHandler(async (req: Request, res: Response) => {
-    try {
-      const result = await prisma.theme.findMany({
-        orderBy: { id: 'asc' },
-      });
+    const result = await prisma.theme.findMany({
+      orderBy: { id: 'asc' },
+    });
 
-      // 테마가 하나도 없을 경우
-      if (!result || result.length === 0) {
-        return errorResponse(res, 404, '해당하는 테마가 없습니다.');
-      }
-
-      return successResponse(res, 200, result, '테마 목록 조회 성공');
-    } catch (error) {
-      // 데이터베이스 오류
-      return errorResponse(res, 500, '데이터베이스 오류입니다.');
+    // 테마가 하나도 없을 경우
+    if (!result || result.length === 0) {
+      return errorResponse(res, 404, '해당하는 테마가 없습니다.');
     }
+
+    return successResponse(res, 200, result, '테마 목록 조회 성공');
   });
 }
 
